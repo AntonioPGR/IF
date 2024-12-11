74fa54df88a564724a2cdd952c5a745891fe0506
Antonio Pacheco <antoninhopgr@gmail.com>
2024-12-11 13:30:03 -0300

    Create 2157.cpp

---
+++ Beecrowd/strings/2157.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int n;
+  cin >> n;
+  while(n--){
+
+    int min, max;
+    cin >> min >> max;
+
+    for(int i = min; i<= max; i++){
+      cout << i;
+    }
+    for(int i = max; i >= min; i--){
+      string number = to_string(i);
+      for(int j = number.size()-1; j >= 0; j--){
+        cout << number.at(j);
+      }
+    }
+    cout << endl;
+  }
+
+}
+
+/*
+
+98 100
+98991001011010019989
+
+*/
